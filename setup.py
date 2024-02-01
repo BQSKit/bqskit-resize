8e6a66d85a770725b825617f19fa932b5bfe0a6c
peachnuts <dandeliondld@163.com>
2024-02-01 11:45:26 -0800

    configure package

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+        name='bqskit-resize',
+        version = '1.0',
+        url = 'https://github.com/BQSKit/bqskit-resize',
+        author = 'Siyuan Niu',
+        author_email = 'siyuanniu@lbl.gov',
+        description = 'Quantum Circuit Resizing Algorithm',
+        install_requires=[
+            'numpy',
+            'bqskit',
+            ],
+)
+
