b586e75c4b963fbe4b36171f64b549a54df69cb9
peachnuts <dandeliondld@163.com>
2024-05-17 09:50:42 -0700

    add qasms

--- examples/circuit_resizing.py
+++ examples/circuit_resizing.py
@@ -7,28 +7,17 @@ from bqskit.ir.gates import CXGate
 from bqskit.ir.gates import HGate
 from bqskit.ir.gates import ZGate
 from bqskit.passes import *
-from resize.gatedeppredicate import ResizingGateDependencyPredicate
-from resize.qfactorpredicate import ResizingQFactorPredicate
-from resize.gatedependencyresize import GateDependencyResize
-from resize.blocklayer import BlockLayerGenerator
+from resize import ResizingGateDependencyPredicate
+from resize import ResizingQFactorPredicate
+from resize import GateDependencyResize
+from resize import BlockLayerGenerator
+from pathlib import Path
 import logging
 
-qc1 = Circuit(4)
-for i in range(3):
-    qc1.append_gate(HGate(), i)
-qc1.append_gate(ZGate(), 3)
-for i in range(3):
-    qc1.append_gate(CXGate(), (i, 3))
-for i in range(4):
-    qc1.append_gate(HGate(), i)
+dir = Path(__file__).parent.parent
 
-qc2 = Circuit(4)
-qc2.append_gate(CXGate(), (0, 3))
-qc2.append_gate(CXGate(), (2, 3))
-qc2.append_gate(CXGate(), (1, 2))
-qc2.append_gate(CXGate(), (0, 1))
-qc2.append_gate(CXGate(), (2, 3))
-
+qc1 = Circuit.from_file(f'{dir}/qasms/exp1.qasm')
+qc2 = Circuit.from_file(f'{dir}/qasms/exp2.qasm')
 
 initial_coupling = [(0, 1), (1, 2), (2, 3)]
 model = MachineModel(qc1.num_qudits, coupling_graph=initial_coupling)
---
+++ resize/__init__.py
@@ -0,0 +1,5 @@
+from .gatedeppredicate import ResizingGateDependencyPredicate
+from .qfactorpredicate import ResizingQFactorPredicate
+from .gatedependencyresize import GateDependencyResize
+from .blocklayer import BlockLayerGenerator
+__all__ = ["ResizingGateDependencyPredicate", "ResizingQFactorPredicate", "GateDependencyResize", "BlockLayerGenerator"]
